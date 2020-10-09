425bf39bcf93f51c3dfc83e630cdc43bbfc96691
DmitriyZubenko <dmytro@printfactory.cloud>
2020-10-09 08:44:00 +0300

    Add project files.

---
+++ FoxmindTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoxmindTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
